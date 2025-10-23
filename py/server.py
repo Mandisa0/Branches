d3710d2e46dc4768bd4c08570a7df44049522dce
Mandisam <mandisam@clc.co.za>
2025-10-23 14:20:15 +0200

    feat: updated server

--- py/server.py
+++ py/server.py
@@ -1,7 +1,12 @@
 import subprocess
 
 try:
-    subprocess.run(["python", "-m", "uvicorn", "main:app", "--reload"], check=True)
+    subprocess.run([
+    "python", "-m", "uvicorn", "main:app",
+    "--reload",
+    "--host", "132.148.181.9",
+    "--port", "7620"
+], check=True)
 except subprocess.CalledProcessError as e:
     print(f"Error running uvicorn: {e}")
 except KeyboardInterrupt:
