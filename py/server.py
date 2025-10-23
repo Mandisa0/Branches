83dc0740681ae8ecb0265eff539a64f5f5b73be4
Mandisa Mngomezulu <lordlemon0@gmail.com>
2025-10-23 17:16:18 +0200

    feat: updated server

--- py/server.py
+++ py/server.py
@@ -5,7 +5,7 @@ try:
     "python", "-m", "uvicorn", "main:app",
     "--reload",
     "--host", "phantomstudio.co.za",
-    "--port", "22"
+    "--port", "8000"
 ], check=True)
 except subprocess.CalledProcessError as e:
     print(f"Error running uvicorn: {e}")
